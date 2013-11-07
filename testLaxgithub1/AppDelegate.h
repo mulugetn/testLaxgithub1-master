587ed31e6959f2f1cbfc7133b5cf9a5c7554637a
Mulugeta <mulugetaalemayehu@yahoo.com>
2013-11-07 11:24:37 -0500

    making changes on lax
    making changes on lax's file

---
+++ testLaxgithub1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testLaxgithub1
+//
+//  Created by Remote Tiger on 11/7/13.
+//  Copyright (c) 2013 Lax Senapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testLaxgithub1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testLaxgithub1
+//
+//  Created by Remote Tiger on 11/7/13.
+//  Copyright (c) 2013 Lax Senapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
